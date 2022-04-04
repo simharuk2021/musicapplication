d8fb9ca03ec680f0b6c4fc73cfe3f38213ab8a9a
simharuk2021 <simharuk@gmail.com>
2022-04-04 14:39:55 +0000

    navbar, index and track templates added

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,10 @@
 from application import app, db
 from application.models import Music
+from flask import render_template, redirect, url_for, request
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 # @app.route('/add')
 # def add():
@@ -8,13 +13,13 @@ from application.models import Music
 #     db.session.commit()
 #     return "Added new game to database"
 
-@app.route('/read')
-def read():
+@app.route('/tracks')
+def tracks():
     all_music = Music.query.all()
-    music_string = ""
-    for music in all_music:
-        music_string += "<br>"+ music.track_name
-    return music_string
+    # music_string = ""
+    # for music in all_music:
+    #     music_string += "<br>"+ music.track_name
+    return render_template('tracks.html', all_music=all_music)
 
 # @app.route('/update/<name>')
 # def update(name):
@@ -22,3 +27,12 @@ def read():
 #     first_game.name = name
 #     db.session.commit()
 #     return first_game.name
+
+# '''create a update route'''
+# def delete(name):
+#     music = Music.query.filter_by(track_name=name).first()
+#     music.track_name = name
+#     db.session.commit()
+#     return music.track_name
+
+# @app.route('/delete/<name>')
